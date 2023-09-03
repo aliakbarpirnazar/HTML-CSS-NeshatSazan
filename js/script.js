b2d1f9fbb66449a8bf41f487054516e92848d33c
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2023-09-03 11:17:56 +0330

    Navbar and responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const crossIcon = document.querySelector('.cross-icon');
-const showMoreMenu = document.querySelector('.show-more-menu');
-const showProductMenu = document.querySelector('#show-product-menu');
+const showMorearticle = document.querySelector('#show-more-article');
+const showMoremenu = document.querySelector('#show-more-menu');
+const showMorelang = document.querySelector('#show-more-lang');
 
 hamburgerIcon.addEventListener('click', function () {
   console.log(hamburgerMenu.style.transform);
@@ -13,24 +14,36 @@ crossIcon.addEventListener('click', function () {
   hamburgerMenu.classList.remove('show-hamburger-menu');
 });
 
-showMoreMenu.addEventListener('mouseenter', function (e) {
+showMorearticle.addEventListener('mouseenter', function (e) {
   const ul = e.target.childNodes[5];
   ul.style.animation = 'showUp 0.35s 1';
   ul.style.display = 'block';
 });
 
-showMoreMenu.addEventListener('mouseleave', function (e) {
+showMorearticle.addEventListener('mouseleave', function (e) {
   const ul = e.target.childNodes[5];
   ul.style.display = 'none';
 });
 
-showProductMenu.addEventListener('mouseenter', function (e) {
-  const div = e.target.childNodes[5];
-  div.style.animation = 'showUp 0.35s 1';
-  div.style.display = 'flex';
+showMoremenu.addEventListener('mouseenter', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.animation = 'showUp 0.35s 1';
+  ul.style.display = 'block';
 });
 
-showProductMenu.addEventListener('mouseleave', function (e) {
-  const div = e.target.childNodes[5];
-  div.style.display = 'none';
+showMoremenu.addEventListener('mouseleave', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.display = 'none';
 });
+
+showMorelang.addEventListener('mouseenter', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.animation = 'showUp 0.35s 1';
+  ul.style.display = 'block';
+});
+
+showMorelang.addEventListener('mouseleave', function (e) {
+  const ul = e.target.childNodes[5];
+  ul.style.display = 'none';
+});
+
